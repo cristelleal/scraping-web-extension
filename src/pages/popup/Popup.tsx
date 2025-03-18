9478889d45499780605831b05915a639726d7c5b
Cristelle <cristelleal@gmail.com>
2025-03-18 15:22:07 +0100

    feat(frontend): add first draft of Popup component

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from '@assets/img/logo.svg';
+import logo from "../../../public/article.png";
 
-export default function Popup() {
+const Popup = () => {
   return (
     <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
       <header className="flex flex-col items-center justify-center text-white">
-        <img src={logo} className="h-36 pointer-events-none animate-spin-slow" alt="logo" />
-        <p>
-          Edit <code>src/pages/popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="text-blue-400"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <p>Popup styled with TailwindCSS!</p>
+        <img src={logo} className="h-24" alt="logo" />
+        <h1 className="text-2xl font-bold">Web scraping page extension ✨</h1>
+        <p className="text-md">We summarize all your articles</p>
+        <button className="mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
+          Summarize now
+        </button>
       </header>
     </div>
   );
-}
+};
+
+export default Popup;
+
